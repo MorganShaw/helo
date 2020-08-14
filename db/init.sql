545fa9a0485ea575b646402e1276b90a05046d17
user-1 <user-1@MacBook-Pro.local>
2020-08-14 10:53:58 -0600

    set up database no dummy data

---
+++ db/init.sql
@@ -0,0 +1,29 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(20),
+    password VARCHAR(20),
+    profile_pic TEXT
+);
+
+CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(45),
+    img TEXT,
+    content TEXT,
+    author_id INT REFERENCES users(id)
+);
+
+ALTER TABLE users
+ALTER COLUMN password TYPE TEXT;
+
+-- Dummy data. Seems like I can't put dummy data into the users table because I can't store a password in the database (unless it's a hash password). I tried putting some awesome dummy data into the posts table (but it didn't work because I can't figure out how to do that with the author_id referencing the user id.)
+INSERT INTO users (title, username, profile_pic)
+VALUES
+
+
+INSERT INTO posts (title, img, content, author_id)
+VALUES
+('Happy Halloween!', 'https://www.google.com/url?sa=i&url=http%3A%2F%2Faop-campus.com%2Fhappy-halloween%2F&psig=AOvVaw1ttAH0Oul-DBeNK9K2hM8t&ust=1597509805560000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLDuoYKSm-sCFQAAAAAdAAAAABAD', 'Happy Halloween, everyone! I hope you all get plenty of candy and no razor blades in your apples.', 2);
+('TBT', 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.ytimg.com%2Fvi%2Fy630dZyiSLs%2Fhqdefault.jpg&imgrefurl=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3Dy630dZyiSLs&tbnid=f8ijwHgslQwwKM&vet=12ahUKEwiLoMnWkpvrAhUJRs0KHbYDBucQMygCegUIARDBAQ..i&docid=l8Xhc_isoIcvnM&w=480&h=360&q=high%20school%20musical%20graduation%20zac%20efron&safe=active&ved=2ahUKEwiLoMnWkpvrAhUJRs0KHbYDBucQMygCegUIARDBAQ', 'Remember when Zac Efron gave a stirring graduation speech? Those were the days. Why does he have such a big beard now?', 1),
+('Major gains today', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fbiomehealthandfitness.com%2Ffitness-programs%2Fgainz%2F&psig=AOvVaw0o4BsqnX9OOWU7jdXYoYMy&ust=1597510149227000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKCf3KOTm-sCFQAAAAAdAAAAABAD', 3);
+
---
+++ server/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const massive = require('massive');
+const bcrypt = require('bcrypt');
+const app = express();
+const ctrl = require('./controller')
+
+const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env;
+
+app.use(express.json());
+
+//Ask: ?ssl=true removed from connection string in .env? I thought we were supposed to, but directions are saying it should be there.
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {
+        rejectUnauthorized: false
+    }
+}).then(db => {
+    app.set('db', db)
+    console.log("Database is connected!")
+}).catch(err => console.log(err))
+
+// app.get('', ctrl.get)
+
+app.listen(SERVER_PORT, () => {
+   console.log(`Server is running on port ${SERVER_PORT}`); 
+})
